0844641063a023d7c66457db73943bac3841416c
Adam Babik <adam.babik@contractors.roche.com>
2013-04-11 16:23:23 +0200

    basic functionality, working prototype: refresh[CSS], eval

--- public/respond.js
+++ public/respond.js
@@ -1,6 +1,32 @@
 (function (__global__, Faye) {
 	'use strict';
 
+	var slice = Array.prototype.slice;
+
+	var urlUtils = {
+		isAbsolute: function (url) {
+			return (/^http(s)?\:\/{2,}|\/{2,}/i).test(url);
+		},
+		hasQueryParam: function (url, param) {
+			return ~url.indexOf(param);
+		}
+	};
+
+	function safeStringify(o) {
+		var cache = [];
+		return JSON.stringify(o, function (key, value) {
+			if (typeof value === 'object' && value !== null) {
+				if (cache.indexOf(value) !== -1) {
+					// Circular reference found, discard key
+					return;
+				}
+				// Store value in our collection
+				cache.push(value);
+			}
+			return value;
+		});
+	}
+
 	function Respond(url) {
 		if (!(this instanceof Respond)) {
 			return new Respond(url);
@@ -11,9 +37,49 @@
 	}
 
 	Respond.prototype.setup = function setup() {
+		var self = this;
 		this.client = new Respond.Client(this.url);
 		this.client.subscribe('/call', function (message) {
 			console.log('[ call ]', message);
+			switch (message.cmd) {
+			case 'refresh': 
+				self.refresh();
+				break;
+			case 'refreshCSS':
+				self.refreshCSS();
+				break;
+			case 'eval':
+				try {
+					self.client.publish('/call/result', { status: 0, result: safeStringify(eval(message.data)) });
+				} catch (e) {
+					self.client.publish('/call/result', { status: 1, result: 'Cannot eval `' + message.data + '`' });
+				}
+				break;
+			}
+		});
+	};
+
+	Respond.prototype.refresh = function refresh() {
+		window.location.reload();
+	};
+
+	Respond.prototype.refreshCSS = function refreshCSS() {
+		var styles = document.querySelectorAll('link[rel="stylesheet"]');
+		if (!styles) {
+			return;
+		}
+		slice.call(styles).forEach(function (style) {
+			var href = style.getAttribute('href'),
+				timestampQueryParam = 'timestamp=' + Date.now();
+
+			if (urlUtils.isAbsolute(href)) {
+				return;
+			}
+
+			style.setAttribute('href', urlUtils.hasQueryParam(href, 'timestamp') ?
+				href.replace(/timestamp\=\d+/i, timestampQueryParam) :
+				href + '?' + timestampQueryParam
+			);
 		});
 	};
 
